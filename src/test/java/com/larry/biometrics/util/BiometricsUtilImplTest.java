0a30ccf549841b863048f856ed04742ac51d9bde
Larryochieng <larryochieng@gmail.com>
2014-07-08 13:03:05 +0300

    add stub for test biometrics function

---
+++ src/test/java/com/larry/biometrics/util/BiometricsUtilImplTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.larry.biometrics.util;
+
+import org.jmock.cglib.MockObjectTestCase;
+
+/**
+ * @author Otieno Lawrence
+ * 
+ */
+public class BiometricsUtilImplTest extends MockObjectTestCase {
+
+	@Override
+	public void setUp() throws Exception {
+
+	}
+
+	public void testRegister() throws Exception {
+
+	}
+
+	public void testVerify() throws Exception {
+
+	}
+}
