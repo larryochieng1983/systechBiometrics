0ae5e5d49d21edf9772faf052c0a5b94cff7574c
Larryochieng <larryochieng@gmail.com>
2014-07-08 13:08:07 +0300

    test stub for fund master query adapter

---
+++ src/test/java/com/larry/biometrics/query/PensionerBioQueryAdapterTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.larry.biometrics.query;
+
+import org.jmock.MockObjectTestCase;
+
+/**
+ * @author Otieno Lawrence
+ * 
+ */
+public class PensionerBioQueryAdapterTest extends MockObjectTestCase {
+
+	@Override
+	public void setUp() {
+
+	}
+
+	public void testSavePensionerInfo() throws Exception {
+
+	}
+
+	public void testGetPensionerInfo() throws Exception {
+
+	}
+
+}
