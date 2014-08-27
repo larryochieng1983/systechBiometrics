809663179f363811d7d101c34a2465293f12ce34
Larryochieng <larryochieng@gmail.com>
2014-08-27 13:49:56 +0300

    fix failing test

--- src/test/java/com/larry/biometrics/query/PensionerBioQueryAdapterTest.java
+++ src/test/java/com/larry/biometrics/query/PensionerBioQueryAdapterTest.java
@@ -7,27 +7,24 @@ import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.client.ProxyFactory;
 import org.jmock.MockObjectTestCase;
 
-import com.larry.biometrics.util.FundMasterConfiguration;
-
 /**
  * @author Otieno Lawrence
  * 
  */
 public class PensionerBioQueryAdapterTest extends MockObjectTestCase {
 
-	private FundMasterConfiguration configuration;
+	private String testUrl = "https://www.google.co.ke";
 
 	@Override
 	public void setUp() {
-		configuration = new FundMasterConfiguration();
 	}
 
 	/** Just test if the Proxy is working */
 	public void testClientWorking() throws Exception {
 		CreatePensionerBioProxy createPensionerBioProxy = ProxyFactory.create(
-				CreatePensionerBioProxy.class, configuration.getUrl());
-		ClientResponse response = createPensionerBioProxy.getPensionerBio("","",
-				"7883737383", "fingerprint_data");
+				CreatePensionerBioProxy.class, testUrl);
+		ClientResponse response = createPensionerBioProxy.getPensionerBio("",
+				"", "7883737383", "fingerprint_data");
 		assertEquals(404, response.getStatus());
 	}
 
